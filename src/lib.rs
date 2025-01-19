0a830b3c7d64d86ef31e5e5b6ec0978f12d285e9
Ote Leo <ote.leo73@gmail.com>
2025-01-19 15:28:55 +0300

    Reloading shaders now

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,22 @@
 //     - drag_end
 //     - mouse_left_pressed
 //     - mouse_left_clicked
-// TODO: add the ability to reload the shader
 mod waker;
 
 use waker::block_on;
 
 use winit::{
 	application::ApplicationHandler,
-	dpi::{PhysicalSize, PhysicalPosition},
-	event::WindowEvent,
-	event_loop::{ActiveEventLoop, EventLoop, EventLoopProxy, ControlFlow},
+	dpi::{PhysicalPosition, PhysicalSize},
+	event::{ElementState, WindowEvent},
+	event_loop::{ActiveEventLoop, ControlFlow, EventLoop, EventLoopProxy},
+	keyboard::{KeyCode, PhysicalKey},
 	window::{Window, WindowId},
 };
 
 use anyhow::Context;
+
+const DEFAULT_SHADER_PATH: &str = "shaders/prelude.wgsl";
 
 #[cfg(target_arch = "wasm32")]
 type Rc<T> = std::rc::Rc<T>;
@@ -43,17 +45,25 @@ struct Graphics {
 	surface_config: wgpu::SurfaceConfiguration,
 	device: wgpu::Device,
 	queue: wgpu::Queue,
+
+	render_pipeline_layout: wgpu::PipelineLayout,
 	render_pipeline: wgpu::RenderPipeline,
 
 	window_data: WindowData,
 	window_data_buffer: wgpu::Buffer,
 	window_data_bind_group: wgpu::BindGroup,
+
+	vertex_shader: wgpu::ShaderModule,
 }
 
 async fn create_graphics(event_loop: &ActiveEventLoop) -> anyhow::Result<Graphics> {
 	let window_attrs = Window::default_attributes();
 
-	let window = Rc::new(event_loop.create_window(window_attrs).context("creating window")?);
+	let window = Rc::new(
+		event_loop
+			.create_window(window_attrs)
+			.context("creating window")?,
+	);
 
 	let instance = wgpu::Instance::default();
 	let surface = instance
@@ -82,22 +92,16 @@ async fn create_graphics(event_loop: &ActiveEventLoop) -> anyhow::Result<Graphic
 		.await
 		.context("requesting wgpu device")?;
 
+	device.push_error_scope(wgpu::ErrorFilter::Validation);
+
 	let size = window.inner_size();
 	let surface_config = surface
 		.get_default_config(&adapter, size.width, size.height)
 		.context("getting default surface configuration")?;
 
 	surface.configure(&device, &surface_config);
-	let swapchain_capabilities = surface.get_capabilities(&adapter);
-	let swapchain_format = swapchain_capabilities.formats[0];
-	
-	let vertex_shader = device.create_shader_module(wgpu::include_wgsl!("../shaders/vertex.wgsl"));
 
-	// let fragment_shader = device.create_shader_module(wgpu::include_wgsl!("../shaders/prelude.wgsl"));
-	let fragment_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-		label: Some("fragment_shader"),
-		source: wgpu::ShaderSource::Wgsl(std::fs::read_to_string("shaders/prelude.wgsl").context("reading prelude shader")?.into()),
-	});
+	let vertex_shader = device.create_shader_module(wgpu::include_wgsl!("../shaders/vertex.wgsl"));
 
 	let window_data_buffer = device.create_buffer(&wgpu::BufferDescriptor {
 		label: Some("window_data_buffer"),
@@ -106,10 +110,10 @@ async fn create_graphics(event_loop: &ActiveEventLoop) -> anyhow::Result<Graphic
 		mapped_at_creation: false,
 	});
 
-	let window_data_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-		label: Some("window_data_group_layout"),
-		entries: &[
-			wgpu::BindGroupLayoutEntry {
+	let window_data_group_layout =
+		device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+			label: Some("window_data_group_layout"),
+			entries: &[wgpu::BindGroupLayoutEntry {
 				binding: 0,
 				visibility: wgpu::ShaderStages::FRAGMENT,
 				ty: wgpu::BindingType::Buffer {
@@ -118,44 +122,34 @@ async fn create_graphics(event_loop: &ActiveEventLoop) -> anyhow::Result<Graphic
 					min_binding_size: None,
 				},
 				count: None,
-			}
-		],
-	});
+			}],
+		});
 
 	let window_data_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
 		label: Some("window_data_bind_group"),
 		layout: &window_data_group_layout,
-		entries: &[
-			wgpu::BindGroupEntry {
-				binding: 0,
-				resource: window_data_buffer.as_entire_binding(),
-			}
-		],
+		entries: &[wgpu::BindGroupEntry {
+			binding: 0,
+			resource: window_data_buffer.as_entire_binding(),
+		}],
 	});
 
-	let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-		label: Some("pipeline_layout"),
-		bind_group_layouts: &[
-			&window_data_group_layout,
-		],
+	let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+		label: Some("render_pipeline_layout"),
+		bind_group_layouts: &[&window_data_group_layout],
 		push_constant_ranges: &[],
 	});
 
 	let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
 		label: Some("render_pipeline"),
-		layout: Some(&pipeline_layout),
+		layout: Some(&render_pipeline_layout),
 		vertex: wgpu::VertexState {
 			module: &vertex_shader,
-			entry_point: Some("main"),
+			entry_point: None,
 			compilation_options: Default::default(),
 			buffers: &[],
 		},
-		fragment: Some(wgpu::FragmentState {
-			module: &fragment_shader,
-			entry_point: Some("main"),
-			compilation_options: Default::default(),
-			targets: &[Some(swapchain_format.into())],
-		}),
+		fragment: None,
 		primitive: Default::default(),
 		multisample: Default::default(),
 		cache: None,
@@ -180,11 +174,15 @@ async fn create_graphics(event_loop: &ActiveEventLoop) -> anyhow::Result<Graphic
 		surface_config,
 		device,
 		queue,
+
+		render_pipeline_layout,
 		render_pipeline,
 
 		window_data,
 		window_data_buffer,
 		window_data_bind_group,
+
+		vertex_shader,
 	})
 }
 
@@ -201,13 +199,13 @@ impl GraphicsBuilder {
 
 	fn build_and_send(&mut self, event_loop: &ActiveEventLoop) {
 		let Some(event_loop_proxy) = self.event_loop_proxy.take() else {
-			// `event_loop_proxy` is already spent - we already constructed `Graphics`
 			return;
 		};
 
 		#[cfg(target_arch = "wasm32")]
 		{
-			let gfx = create_graphics(event_loop);
+			let mut gfx = create_graphics(event_loop);
+			gfx.reload_shader().expect("failed to reload shader");
 			wasm_bindgen_futures::spawn_local(async move {
 				let gfx = gfx.await.expect("failed to create graphics context");
 				assert!(event_loop_proxy.send_event(gfx).is_ok());
@@ -215,9 +213,112 @@ impl GraphicsBuilder {
 		}
 		#[cfg(not(target_arch = "wasm32"))]
 		{
-			let gfx = block_on(create_graphics(event_loop)).expect("failed to create graphics context");
+			let mut gfx =
+				block_on(create_graphics(event_loop)).expect("failed to create graphics context");
+			gfx.reload_shader().expect("failed to reload shader");
 			assert!(event_loop_proxy.send_event(gfx).is_ok());
 		}
+	}
+}
+
+impl Graphics {
+	fn draw(&mut self) {
+		let frame = self.surface.get_current_texture().unwrap();
+		let view = frame.texture.create_view(&Default::default());
+		let mut encoder = self.device.create_command_encoder(&Default::default());
+
+		{
+			let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+				color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+					view: &view,
+					resolve_target: None,
+					ops: wgpu::Operations {
+						load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+						store: wgpu::StoreOp::Store,
+					},
+				})],
+				..Default::default()
+			});
+
+			render_pass.set_bind_group(0, Some(&self.window_data_bind_group), &[]);
+			render_pass.set_pipeline(&self.render_pipeline);
+			render_pass.draw(0..3, 0..1);
+		}
+
+		let command_buffer = encoder.finish();
+
+		self.queue
+			.write_buffer(&self.window_data_buffer, 0 as wgpu::BufferAddress, unsafe {
+				core::slice::from_raw_parts(
+					[self.window_data].as_ptr() as *const u8,
+					std::mem::size_of::<WindowData>(),
+				)
+			});
+		self.queue.submit([command_buffer]);
+		frame.present();
+
+		self.window_data.frame = self.window_data.frame.wrapping_add(1);
+	}
+
+	fn resize(&mut self, size: PhysicalSize<u32>) {
+		self.surface_config.width = size.width;
+		self.surface_config.height = size.height;
+		self.window_data.resolution = [size.width as f32, size.height as f32];
+		self.surface.configure(&self.device, &self.surface_config);
+	}
+
+	fn update_cursor_position(&mut self, position: PhysicalPosition<f64>) {
+		self.window_data.cursor = [position.x as f32, position.y as f32];
+	}
+
+	fn reload_shader(&mut self) -> anyhow::Result<()> {
+		let shader_src = std::fs::read_to_string(DEFAULT_SHADER_PATH)
+			.context("reading vertex prelude shader")?;
+		let shader = self
+			.device
+			.create_shader_module(wgpu::ShaderModuleDescriptor {
+				label: Some("shader"),
+				source: wgpu::ShaderSource::Wgsl(shader_src.into()),
+			});
+
+		block_on(async {
+			let compilation_info = shader.get_compilation_info().await;
+			for msg in compilation_info.messages.iter() {
+				if msg.message_type != wgpu::CompilationMessageType::Info {
+					eprintln!("{}", msg.message);
+				}
+			}
+		});
+
+		self.render_pipeline =
+			self.device
+				.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+					label: Some("render_pipeline"),
+					layout: Some(&self.render_pipeline_layout),
+					vertex: wgpu::VertexState {
+						module: &self.vertex_shader,
+						entry_point: None,
+						compilation_options: wgpu::PipelineCompilationOptions::default(),
+						buffers: &[],
+					},
+					primitive: wgpu::PrimitiveState::default(),
+					depth_stencil: None,
+					multisample: wgpu::MultisampleState::default(),
+					fragment: Some(wgpu::FragmentState {
+						module: &shader,
+						entry_point: None,
+						compilation_options: wgpu::PipelineCompilationOptions::default(),
+						targets: &[Some(wgpu::ColorTargetState {
+							format: self.surface_config.format,
+							blend: Some(wgpu::BlendState::REPLACE),
+							write_mask: wgpu::ColorWrites::ALL,
+						})],
+					}),
+					multiview: None,
+					cache: None,
+				});
+
+		Ok(())
 	}
 }
 
@@ -237,67 +338,11 @@ impl Application {
 		}
 	}
 
-	fn update_cursor_position(&mut self, position: PhysicalPosition<f64>) {
-		let MaybeGraphics::Graphics(gfx) = &mut self.graphics else {
-			return;
-		};
-		gfx.window_data.cursor = [position.x as f32, position.y as f32];
-	}
-
 	fn draw(&mut self) {
 		let MaybeGraphics::Graphics(gfx) = &mut self.graphics else {
 			return;
 		};
-
-		let frame = gfx.surface.get_current_texture().unwrap();
-		let view = frame.texture.create_view(&Default::default());
-		let mut encoder = gfx.device.create_command_encoder(&Default::default());
-
-		{
-			let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-				color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-					view: &view,
-					resolve_target: None,
-					ops: wgpu::Operations {
-						load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
-						store: wgpu::StoreOp::Store,
-					},
-				})],
-				..Default::default()
-			});
-
-			render_pass.set_bind_group(0, Some(&gfx.window_data_bind_group), &[]);
-			render_pass.set_pipeline(&gfx.render_pipeline);
-			render_pass.draw(0..3, 0..1);
-		}
-
-		let command_buffer = encoder.finish();
-
-		gfx.queue.write_buffer(
-			&gfx.window_data_buffer,
-			0 as wgpu::BufferAddress,
-			unsafe {
-				core::slice::from_raw_parts(
-					[gfx.window_data].as_ptr() as *const u8,
-					std::mem::size_of::<WindowData>(),
-				)
-			},
-		);
-		gfx.queue.submit([command_buffer]);
-		frame.present();
-
-		gfx.window_data.frame = gfx.window_data.frame.wrapping_add(1);
-	}
-
-	fn resize(&mut self, size: PhysicalSize<u32>) {
-		let MaybeGraphics::Graphics(gfx) = &mut self.graphics else {
-			return;
-		};
-
-		gfx.surface_config.width = size.width;
-		gfx.surface_config.height = size.height;
-		gfx.window_data.resolution = [size.width as f32, size.height as f32];
-		gfx.surface.configure(&gfx.device, &gfx.surface_config);
+		gfx.draw()
 	}
 }
 
@@ -305,7 +350,7 @@ impl ApplicationHandler<Graphics> for Application {
 	fn window_event(
 		&mut self,
 		event_loop: &ActiveEventLoop,
-		_window_id: WindowId,
+		window_id: WindowId,
 		event: WindowEvent,
 	) {
 		use winit::{
@@ -313,8 +358,16 @@ impl ApplicationHandler<Graphics> for Application {
 			keyboard::{Key, NamedKey},
 		};
 
+		let MaybeGraphics::Graphics(gfx) = &mut self.graphics else {
+			return;
+		};
+
+		if gfx.window.id() != window_id {
+			return;
+		}
+
 		match event {
-			WindowEvent::Resized(size) => self.resize(size),
+			WindowEvent::Resized(size) => gfx.resize(size),
 			WindowEvent::CloseRequested
 			| WindowEvent::KeyboardInput {
 				event: KeyEvent {
@@ -323,7 +376,16 @@ impl ApplicationHandler<Graphics> for Application {
 				},
 				..
 			} => event_loop.exit(),
-			WindowEvent::CursorMoved { position, .. } => self.update_cursor_position(position),
+			WindowEvent::CursorMoved { position, .. } => gfx.update_cursor_position(position),
+			WindowEvent::KeyboardInput {
+				event:
+					KeyEvent {
+						physical_key: PhysicalKey::Code(KeyCode::KeyR),
+						state: ElementState::Pressed,
+						..
+					},
+				..
+			} => gfx.reload_shader().expect("failed to reload shader"),
 			_ => (),
 		}
 	}
